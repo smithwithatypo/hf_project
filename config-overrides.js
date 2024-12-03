b248e0cd4868791475ea4eaa85d9699eec9ca81e
Tom Li <tomli1@utexas.edu>
2024-12-02 19:10:56 -0600

    Fix frontend container build fail

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// config-overrides.js
+
+module.exports = function override(config) {
+    // Fallback for Node.js core modules
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
+      path: require.resolve('path-browserify'),
+    };
+    return config;
+  };
